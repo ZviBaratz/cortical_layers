51218f2fca62e76234998578d13f6c4b9e4ded55
Zvi Baratz <z.baratz@gmail.com>
2018-05-31 22:41:44 +0300

    Fixed multiplanar slice viewing

--- app/helpers.py
+++ app/helpers.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+from bokeh.models import ColumnDataSource
 from bokeh.models.widgets import Div
 from typing import Iterable
 
@@ -11,19 +12,33 @@ class AllClassAALHandler:
     Mean: {}<br />
     STD: {}<br />
     '''
-    info_functions = {'min', 'max', 'mean', 'std'}
+    info_functions = ['min', 'max', 'mean', 'std']
+    slice_planes = ['sagittal', 'coronal', 'horizontal']
 
     def __init__(self, image: np.ndarray):
         self.image = image
 
     def get_sagittal_slice(self, slice: int, cortical_class: int):
-        return np.fliplr(np.transpose(self.image[slice, :, :, cortical_class]))
+        return np.fliplr(np.rot90(self.image[slice, :, :, cortical_class], 3))
 
     def get_coronal_slice(self, slice: int, cortical_class: int):
-        return self.image[:, slice, :, cortical_class]
+        return np.rot90(self.image[:, slice, :, cortical_class], 3)
 
     def get_horizontal_slice(self, slice: int, cortical_class: int):
-        return np.transpose(self.image[:, :, slice, cortical_class])
+        return np.rot90(self.image[:, :, slice, cortical_class], 3)
+
+    def get_all_planes_by_coords(self, i_sagittal_slice: int, i_coronal_slice: int,
+                                 i_horizontal_slice: int, cortical_class: int):
+        return [self.get_sagittal_slice(i_sagittal_slice, cortical_class),
+                self.get_coronal_slice(i_coronal_slice, cortical_class),
+                self.get_horizontal_slice(i_horizontal_slice, cortical_class)]
+
+    def get_columndatasources_by_coords(self, i_sagittal_slice: int, i_coronal_slice: int,
+                                        i_horizontal_slice: int, cortical_class: int):
+        slices = self.get_all_planes_by_coords(i_sagittal_slice, i_coronal_slice,
+                                               i_horizontal_slice, cortical_class)
+        slices = [ColumnDataSource(dict(image=[slice])) for slice in slices]
+        return dict(zip(self.slice_planes, slices))
 
     def create_slice_info_div(self, slice: np.ndarray):
         info = self.get_formatted_slice_info(slice)
--- app/main.py
+++ app/main.py
@@ -11,50 +11,55 @@ from helpers import AllClassAALHandler
 data = np.load('/home/flavus/PycharmProjects/cortical_layers/app/all_class_means_aal.npy')
 
 # Create an image wrapper object to easily generate slices
-image = AllClassAALHandler(data)
-
-# Create a data source for the start slice
-start_slice = image.get_coronal_slice(slice=0, cortical_class=0)
-source = ColumnDataSource(data=dict(image=[start_slice]))
-
-# Create slice and class sliders
-slice_slider = Slider(start=1, end=100, value=1, step=1, title="Slice")
-class_slider = Slider(start=1, end=6, value=1, step=1, title="Class")
+aal_handler = AllClassAALHandler(data)
+i_sagittal_slice = 100
+i_coronal_slice = 100
+i_horizontal_slice = 100
+cortical_class = 1
+sources = aal_handler.get_columndatasources_by_coords(i_sagittal_slice,
+                                                      i_coronal_slice,
+                                                      i_horizontal_slice,
+                                                      cortical_class)
 
 # Create an info paragraph next to the image
-info_div = image.create_slice_info_div(start_slice)
+# info_div = image.create_slice_info_div(start_slice)
 
 
-def plot_slice(slice: np.ndarray):
-    plot = bp.figure(plot_width=slice.shape[0] * 3,
-                     plot_height=slice.shape[1] * 3,
-                     x_range=[0, slice.shape[0]],
-                     y_range=[0, slice.shape[1]],
+def plot_slice(plane: str):
+    source = sources[plane]
+    slice = source.data['image'][0]
+    plot = bp.figure(plot_width=slice.shape[1] * 2,
+                     plot_height=slice.shape[0] * 2,
+                     x_range=[0, slice.shape[1]],
+                     y_range=[0, slice.shape[0]],
+                     title=f'{plane.capitalize()} View',
                      tools=[
                          HoverTool(tooltips=[("x", "$x"), ("y", "$y"), ("value", "@image")])])
-    plot.image(image='image', x=0, y=0, dw=slice.shape[0], dh=slice.shape[1],
+    plot.image(image='image', x=0, y=0, dw=slice.shape[1], dh=slice.shape[0],
                source=source, palette='Spectral11')
     return plot
 
 
-def update_slice(attr, old, new):
-    new_image = image.get_coronal_slice(slice=slice_slider.value - 1,
-                                        cortical_class=class_slider.value - 1)
-    source.data = dict(image=[new_image])
-    info_div.text = image.get_formatted_slice_info_template(new_image)
+def update_sources(i_sagittal_slice: int, i_coronal_slice: int,
+                   i_horizontal_slice: int, cortical_class: int):
+    sources = aal_handler.get_columndatasources_by_coords(i_sagittal_slice,
+                                                          i_coronal_slice,
+                                                          i_horizontal_slice,
+                                                          cortical_class)
 
 
-def update_class(attr, old, new):
-    new_image = image.get_coronal_slice(slice=slice_slider.value - 1,
-                                        cortical_class=class_slider.value - 1)
-    source.data = dict(image=[new_image])
-    info_div.text = image.get_formatted_slice_info_template(new_image)
+def plot_multi_planar(i_sagittal_slice: int, i_coronal_slice: int,
+                      i_horizontal_slice: int, cortical_class: int):
+    # update_sources(i_sagittal_slice,
+    #                i_coronal_slice,
+    #                i_horizontal_slice,
+    #                cortical_class)
+    return [plot_slice(plane) for plane in aal_handler.slice_planes]
 
 
-# Set slider handlers
-slice_slider.on_change('value', update_slice)
-class_slider.on_change('value', update_class)
-
-final = gridplot([plot_slice(start_slice), info_div], [widgetbox(slice_slider, class_slider)])
+# final = gridplot([plot_slice(start_slice), info_div], [widgetbox(slice_slider, class_slider)])
+multi_planar_plot = plot_multi_planar(i_sagittal_slice, i_coronal_slice, i_horizontal_slice,
+                                      cortical_class)
+final = gridplot([multi_planar_plot])
 
 curdoc().add_root(final)
